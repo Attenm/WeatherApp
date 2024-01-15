e60ede601f26791b033e81f8c511a171c5390950
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-01-15 09:06:56 +0200

    fixed access to info if location vas denied
    fixed access to info if location was denied

--- scripts/modules/location.js
+++ scripts/modules/location.js
@@ -5,22 +5,24 @@ class Location {
     }
 
     getUserLocation() {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve)=>{
             if('geolocation' in this.navigator){
                 navigator.geolocation.getCurrentPosition((position)=>{
                     this.latitude = position.coords.latitude;
                     this.longtitude = position.coords.longitude;
-                    console.log(this.latitude);
-                    console.log(this.longtitude);
                     resolve();
-                }, reject);
+                }, () => {
+                    this.latitude = 49.0962944;
+                    this.longtitude = 33.4331904;
+                    resolve();
+                });
             } else {
                 this.latitude = 49.0962944;
                 this.longtitude = 33.4331904;
                 resolve();
             }
-        })
-    }
+            })
+        }
 }
 
 const location = new Location();
--- scripts/modules/requestHandler.js
+++ scripts/modules/requestHandler.js
@@ -4,21 +4,21 @@ import location from "./location.js";
 export default class RequestHandler {
 
     getData(requestType) {
-        return location.getUserLocation()
-            .then(()=>{
-                const requestUrl = `${url}${requestType}?lat=${location.latitude}&lon=${location.longtitude}&appid=${apiKey}`;
-                return new Promise((resolve, reject) => {
-                    fetch(requestUrl)
-                        .then((response) => {
-                            return response.json();
-                        })
-                        .then (data => {
-                            resolve(data);
-                        })
-                        .catch(error => {
-                            reject(error);
-                        })
-                });
+        return location.getUserLocation().then(()=>{
+            
+            const requestUrl = `${url}${requestType}?lat=${location.latitude}&lon=${location.longtitude}&appid=${apiKey}`;
+            return new Promise((resolve, reject) => {
+                fetch(requestUrl)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then (data => {
+                        resolve(data);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    })
             });
+        });
     }
 }
